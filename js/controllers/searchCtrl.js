94902f19baafdb1e895ce43aa9ab313d276490a6
CdrXander <xackhackman@gmail.com>
2016-12-05 16:52:12 -0700

    Button toggle working on the search page.

--- js/controllers/searchCtrl.js
+++ js/controllers/searchCtrl.js
@@ -1,11 +1,26 @@
 angular.module('movieShelf').controller('searchCtrl', function($scope, omdbService, localStorageService) {
-	$scope.test = "Test Search Ctrl";
 
+	$scope.reloadMovies = function() {
+		var savedMovies = localStorageService.getSavedMovies();
+		for(var i = 0; i < $scope.movies.length;i++) {
+			for(var j = 0; j < savedMovies.length; j++) {
+				if($scope.movies[i].imdbID == savedMovies[j].imdbID) {
+					$scope.movies[i].own = savedMovies[j].own;
+					$scope.movies[i].watch = savedMovies[j].watch;
+					break;
+				}
+			}
+		}
+		// $scope.movies = $scope.movies
+	}
 
 	$scope.searchForMovie = function() {
 		omdbService.searchForMovie($scope.searchTitle).then(function(serviceData) {
 			$scope.movies = serviceData;
-			console.log($scope.movies);
+			$scope.reloadMovies();
 		})
 	}
+
+
+	
 });
